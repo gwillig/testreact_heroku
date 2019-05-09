c04c82d0afa6e80cb1c68ee79d7923da7067b9f5
gwillig <gustavwillig@gmail.com>
2019-05-09 17:10:31 +0200

    changed settings

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -51,6 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'reactdjango.urls'
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
 
 TEMPLATES = [
   {
@@ -58,6 +64,7 @@ TEMPLATES = [
     'DIRS': [
     os.path.join(BASE_DIR, 'build'),
     os.path.join(BASE_DIR, '../build'),
+    root('build')
 
     ],
     'APP_DIRS': True,
